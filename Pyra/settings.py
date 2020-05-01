8f8268b3f10547be632da650862b1f43e199abd9
barchuckie <patrykbarczak@outlook.com>
2020-05-01 09:44:37 +0200

    email sending config

--- Pyra/settings.py
+++ Pyra/settings.py
@@ -120,6 +120,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# User login
+
 LOGIN_URL = '/accounts/login'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# Email sending
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
