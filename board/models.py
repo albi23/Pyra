b328770de52a39cf9280cfa1daf3505a49b2cbbb
barchuckie <patrykbarczak@outlook.com>
2020-05-17 21:08:25 +0200

    add Membership M2M relation

--- board/models.py
+++ board/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -5,9 +6,16 @@ from django.db import models
 class Board(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=200)
+    members = models.ManyToManyField(User, through='Membership')
 
     def __str__(self):
         return self.name
+
+
+class Membership(models.Model):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    join_date = models.DateTimeField(auto_now_add=True)
 
 
 class Task(models.Model):
@@ -15,6 +23,8 @@ class Task(models.Model):
     description = models.CharField(max_length=200)
     status = models.CharField(max_length=5)  # TODO DOING DONE
     board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
