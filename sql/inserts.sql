9cd5bc838475d3cf34373399426a56a6250cc722
Albert PIekielny <aolo23@wp.pl>
2020-05-18 22:30:31 +0200

    Sql files v2
    Signed-off-by: Albert PIekielny <aolo23@wp.pl>

--- sql/inserts.sql
+++ sql/inserts.sql
@@ -1,3 +1,24 @@
 insert into board_board values(1,'Test board','Testing board ');
+insert into board_board values(2,'Test board 2','Testing board 2');
+insert into board_board values(3,'Test board 3','Testing board 3');
+insert into board_board values(4,'Test board 4','Testing board 4');
+insert into board_board values(5,'Test board 5','Testing board 5');
+insert into board_board values(6,'Test board 6','Testing board 6');
 insert into board_membership values (1,current_date,1,1);
-insert into board_task values(1,'Finish this fucking project','Everything needs to be done','TODO',1,current_date,current_date)
+insert into board_membership values (2,current_date,1,1);
+insert into board_membership values (3,current_date,1,1);
+insert into board_membership values (4,current_date,1,1);
+insert into board_membership values (5,current_date,1,1);
+insert into board_membership values (6,current_date,1,1);
+insert into board_task values(1,'Finish this fucking project','Everything needs to be done','TODO',1,current_date,current_date);
+insert into board_task values(2,'Finish this fucking project 1','Everything needs to be done 1','TODO',1,current_date,current_date);
+insert into board_task values(3,'Finish this fucking project 2','Everything needs to be done 2','TODO',1,current_date,current_date);
+insert into board_task values(4,'Finish this fucking project 3','Everything needs to be done 3','TODO',1,current_date,current_date);
+insert into board_task values(5,'Finish this fucking project 4','Everything needs to be done 4','TODO',1,current_date,current_date);
+
+insert into board_task values(6,'#2 Finish this fucking project','#2 Everything needs to be done','TODO',2,current_date,current_date);
+insert into board_task values(7,'#2 Finish this fucking project 1','#2 Everything needs to be done 1','TODO',2,current_date,current_date);
+insert into board_task values(8,'#2 Finish this fucking project 2','#2 Everything needs to be done 2','TODO',2,current_date,current_date);
+insert into board_task values(9,'#2 Finish this fucking project 3','#2 Everything needs to be done 3','TODO',2,current_date,current_date);
+insert into board_task values(10,'#2 Finish this fucking project 4','#2 Everything needs to be done 4','TODO',2,current_date,current_date);
+
