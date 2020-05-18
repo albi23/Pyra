b06cf4196ed25579693f82becc488701a2745171
Albert PIekielny <aolo23@wp.pl>
2020-05-18 22:15:58 +0200

    Sql files
    Signed-off-by: Albert PIekielny <aolo23@wp.pl>

---
+++ sql/inserts.sql
@@ -0,0 +1,3 @@
+insert into board_board values(1,'Test board','Testing board ');
+insert into board_membership values (1,current_date,1,1);
+insert into board_task values(1,'Finish this fucking project','Everything needs to be done','TODO',1,current_date,current_date)
