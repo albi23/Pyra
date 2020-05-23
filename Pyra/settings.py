e6d9a3fec0b002a5ab3d4ba5cd26e5f688ab14c9
barchuckie <patrykbarczak@outlook.com>
2020-05-23 12:37:15 +0200

    fix of login redirect

--- Pyra/settings.py
+++ Pyra/settings.py
@@ -124,7 +124,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # User login
 
 # LOGIN_URL = '/accounts/login/'
-# LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 # Email sending
