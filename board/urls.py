25aee7e0a3687c48f0b277c28fbd93f0ad71d41c
Kuba <jakub.dochniak@protonmail.com>
2020-06-03 23:53:48 +0200

    Create view which receives new task POST request data

--- board/urls.py
+++ board/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('board/<int:board_id>/', views.board, name='board'),
     path('signup/', views.SignUp.as_view(), name='signup'),
-    path('update-task/', views.update_task_state)
+    path('update-task/', views.update_task_state),
+    path('create-task/', views.create_task)
 ]
